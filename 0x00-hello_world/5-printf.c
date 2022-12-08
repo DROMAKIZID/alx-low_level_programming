f182ed5248e70c46fce54b5176d4f00d9151e572
DROMAKIZID <dromakizito@gmail.com>
2022-12-08 06:18:47 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("my naame is patoo 2020\n");
+
+			return (0);
+
+}
