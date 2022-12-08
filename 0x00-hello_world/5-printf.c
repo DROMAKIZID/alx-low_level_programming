af784b7e2bc3d56400ce899d4991ec20a908094c
DROMAKIZID <dromakizito@gmail.com>
2022-12-08 12:29:26 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
-
-
-/**
- 
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+   /**
+       * main - Entry point
+       *
+       * Return: Always 0 (Success)
+       */
 int main(void)
 
 {
-		printf("with programming you enjoy yourself,\n")
+		printf("programming is so amazing,\n");
 			return (0);
 }
